67afa7da1e267f785f8ba7e88e496d1cb9c0c66e
okey573 <854647481@qq.com>
2024-04-26 11:10:40 +0800

    style: 移除无用声明

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,4 +1,4 @@
-chrome.action.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function () {
   const url = chrome.runtime.getURL('workbench.html')
   // TODO 优化：判断如果已存在打开的tab页，则不再重复打开而是定位到已打开的tab页
   chrome.tabs.create({ url })
