b4df22839125332bfd5bb6c7ab188651842fa0b5
okey573 <854647481@qq.com>
2024-04-30 23:10:01 +0800

    feat: 简单封装 MenuButton

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,7 +1,26 @@
 import { Button, ConfigProvider, Flex, } from 'antd'
-import { UngroupOutlined, DesktopOutlined } from '@ant-design/icons'
+import { UngroupOutlined, DesktopOutlined, AlertOutlined } from '@ant-design/icons'
 import { emitEvent } from '@/service-worker/runtime-message-center.ts'
 import { RUN_TIME_EVENT } from '@/enums.ts'
+import React from 'react'
+import { ButtonType } from 'antd/es/button'
+
+const MenuButton: React.FC<{
+  children: React.ReactNode,
+  icon: React.ReactNode,
+  onClick: () => void,
+  type?: ButtonType
+}> = ({ children, icon, onClick, type = 'text' }) => {
+  return <Button
+    block
+    type={type}
+    style={{ borderRadius: '0' }}
+    icon={icon}
+    onClick={onClick}
+  >
+    {children}
+  </Button>
+}
 
 function App() {
   const changeGroupedTabs = () => emitEvent(RUN_TIME_EVENT.CHANGE_GROUP_TABS)
@@ -9,22 +28,9 @@ function App() {
 
   return <ConfigProvider theme={{ token: { colorPrimary: 'rgb(94, 92, 230)' } }}>
     <Flex vertical gap="small" style={{ width: '200px' }}>
-
-      <Button block
-              type="text"
-              style={{ borderRadius: '0' }}
-              icon={<UngroupOutlined />}
-              onClick={changeGroupedTabs}>
-        分组（取消）标签页
-      </Button>
-
-      <Button block
-              type="primary"
-              style={{ borderRadius: '0' }}
-              icon={<DesktopOutlined />}
-              onClick={openWorkbench}>
-        打开工作台
-      </Button>
+      <MenuButton icon={<AlertOutlined />} onClick={() => void 0}>还没想好做什么</MenuButton>
+      <MenuButton icon={<UngroupOutlined />} onClick={changeGroupedTabs}>分组（取消）标签页</MenuButton>
+      <MenuButton type="primary" icon={<DesktopOutlined />} onClick={openWorkbench}>打开工作台</MenuButton>
     </Flex>
   </ConfigProvider>
 }
